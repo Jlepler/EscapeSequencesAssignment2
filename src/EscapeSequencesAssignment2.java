94035588818b7922bf2cf31c7cf970b37edd576a
jlepler <jlepler@montgomerycollege.edu>
2019-06-12 16:05:39 -0400

    Initial commit of EscapeSequencesAssignment2

---
+++ src/EscapeSequencesAssignment2.java
@@ -0,0 +1,12 @@
+public class EscapeSequencesAssignment2 {
+    public static void main(String [] args){
+
+        System.out.println("Cost\t  Quantity\t  Total\t" +
+                "\n$1,000.00\t4\t\t$4,000.00" +
+                "\n$\t50.00\t8\t\t  $400.00" +
+                "\n" +
+                "\n______________________________" +
+                "\nTOTAL:\t\t12\t\t$4,400.00");
+
+    }
+}
